e213976bd0e809fff2e4e5f96bc4460ec934fb2f
shadab1995 <shadabazama@gmail.com>
2025-02-19 17:48:31 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <h1> helloworld </h1>
+    <h2> my React app deply on EC2 AWS</h2>
+   </div>
+  )
+}
+
+export default App
