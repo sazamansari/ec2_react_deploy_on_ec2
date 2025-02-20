89431b49ea53593ba81e70db2ac523edf3f52aba
shadab1995 <shadabazama@gmail.com>
2025-02-20 21:14:00 +0530

    my first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div>
-    <h1> helloworld </h1>
-    <h2> my React app deply on EC2 AWS</h2>
-   </div>
+    <div>
+      <h1>Hello world</h1>
+      <h2> My ReactJS App host on EC2</h2>
+      <p> Deploy by Md Shadab Azam Ansari</p>
+    </div>
+   
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server :{
-  port : 3000,
- host : true
-  
-}})
+  server : {
+    port : 3000,
+    host: true
+  }
+
+})
